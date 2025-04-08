2094fb1675fba1c4090083b7e299a6f190dd1f11
drashtipatel2804 <21bmiitbscit003@gmail.com>
2025-04-08 15:56:27 +0200

    update in helloworld

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,11 @@
 int main()
 {
     printf("Hey Drashti");
+    int num=25;
+    char star='*';
+    float pi=3.14; 
+    int a=30;
+    int A=34; //varialble are case sensitive
     return 0;
     
 }
